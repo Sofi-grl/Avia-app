edb925f711ef48990b3c37f5704df11b631dd640
Sofia Syvolapenko <syvolapenkosofia28@gmail.com>
2024-05-01 01:08:15 +0300

    made changes for correct compilation on netlify

--- src/components/FlightOptions.tsx
+++ src/components/FlightOptions.tsx
@@ -28,12 +28,7 @@ interface FlightOption {
   returnRoute: FlightRoute;
 }
 
-const formatTime = (time: string): string => {
-  const [hours, minutes] = time.split(':');
-  const formattedHours = hours.padStart(2, '0');
-  const formattedMinutes = minutes.padStart(2, '0');
-  return `${formattedHours}:${formattedMinutes}`;
-};
+
 
 const FlightOptions: React.FC = () => {
   const [visibleOptions, setVisibleOptions] = useState<number>(5);
